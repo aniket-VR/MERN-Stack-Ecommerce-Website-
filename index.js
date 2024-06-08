109f472cef0b2f2ab2af347738dfac5b8e8595a5
aniket <aniketrohokale2002@gmail.com>
2024-06-08 22:18:40 +0530

    update cors origin acces

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ connectMongodb(process.env.DB_URL).then(() => {
 });
 app.use(
   cors({
-    origin: ["http://localhost:5173"], // Allow all origins
+    origin: "*", // Allow all origins
     methods: ["GET", "POST", "PUT", "DELETE"], // Allow specific methods
     allowedHeaders: ["Content-Type", "Authorization"],
   })
